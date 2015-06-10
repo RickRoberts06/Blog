57696047763cf8c0910dd6727928a0406e2596c0
Eric Carpentier <ecarpentier007@gmail.com>
2015-06-10 16:54:02 -0700

    Fixed the delete link

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   root "pods#index"
 
-  get "/pods" => "pods#new"
-  post "/pods" => "pods#create"
-
-  get "/pods/:id" => "pods#edit", as: :pod
-  patch "/pods/:id" => "pods#udpate"
-
-  get "/pods/:id" => "pods#destroy", as: :delete
-
+  resources :pods
 
 
 end
