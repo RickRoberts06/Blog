e782648d1ebb42619c8be839cdfa86fb47a7484a
Eric Carpentier <ecarpentier007@gmail.com>
2015-07-03 09:09:04 -0700

    Added mailer for comments"

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "dye_06@hotmail.com"
+  layout 'mailer'
+end
---
+++ config/initializers/app_keys.rb
@@ -0,0 +1,2 @@
+ruby ENV["email_user_name"] = "dye_06@hotmail.com"
+ENV["email_password"] = "$$060dantes"
